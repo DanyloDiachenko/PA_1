c6ad9011534cb1ec349b823da1a65e775718625a
Danil <danildiachenko23@gmail.com>
2025-10-30 14:11:05 +0200

    init

---
+++ lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿// природне злиття,
+// латинська літера (ключ) - рядок довжиною до 45 символів - номер телефону
+// зростання
+// доступний об'єм - 300мб
+
+namespace Lab1
+{
+  public class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
